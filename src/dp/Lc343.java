bdb7558f5f67831c076209486bae6f5e6343b09c
wangruicong <juictungwong@outlook.com>
2021-10-01 21:19:50 +0800

    整数拆分

--- src/dp/Lc343.java
+++ src/dp/Lc343.java
@@ -1,2 +1,13 @@
-package dp;public class Lc343 {
+package dp;
+
+public class Lc343 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int integerBreak(int n) {
+        return 0;
+    }
 }
