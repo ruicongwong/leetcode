914a06d0a442fc5f25fa925e34b6fdbc92d5ba84
wangruicong <juictungwong@outlook.com>
2021-10-30 21:10:55 +0800

    跳跃游戏

---
+++ src/greedy/JumpGame.java
@@ -0,0 +1,17 @@
+package greedy;
+
+public class JumpGame {
+
+}
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        if (nums.length == 1) return true;
+        int index = 0;
+        for (int i = 0; i <= index; i++) {
+           index = Math.max(index, i + nums[i]);
+           if(index >= nums.length - 1)return true;
+        }
+        return false;
+    }
+}
