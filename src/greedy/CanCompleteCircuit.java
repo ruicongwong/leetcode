08650ab069ad04b0f1aa0d58eec252240cfd9cf9
wangruicong <juictungwong@outlook.com>
2021-11-05 20:36:23 +0800

    加油站

---
+++ src/greedy/CanCompleteCircuit.java
@@ -0,0 +1,25 @@
+package greedy;
+
+import java.util.stream.IntStream;
+
+public class CanCompleteCircuit {
+}
+
+
+class Solution_134 {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int gasSum = IntStream.of(gas).sum();
+        int costSum = IntStream.of(cost).sum();
+        if (costSum > gasSum) return -1;
+        int tank = 0;
+        int start = 0;
+        for (int i = 0; i < gas.length; i++) {
+            tank += gas[i] - cost[i];
+            if(tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        return start;
+    }
+}
