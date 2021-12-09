40f71c4977a5cbfef8eda58905725049cfb2d2fc
ruicong <juictungwong@outlook.com>
2021-12-09 21:43:19 +0800

    颜色分类

---
+++ src/hot100/SortColors.java
@@ -0,0 +1,30 @@
+package hot100;
+
+public class SortColors {
+}
+
+class Solution75 {
+    public void sortColors(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int i = 0;
+        while (i <= right) {
+            if (nums[i] == 0) {
+                swap(nums, i, left);
+                left ++;
+                i++;
+            } else if (nums[i] == 2) {
+                swap(nums, i, right);
+                right --;
+            } else {
+                i++;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
