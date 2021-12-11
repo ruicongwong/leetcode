a4c32413a11dc2b8bdb78229705d663fe46ab2d7
ruicong <juictungwong@outlook.com>
2021-12-11 16:38:12 +0800

    公有化变量

--- src/binaryTree/TreeNode.java
+++ src/binaryTree/TreeNode.java
@@ -1,9 +1,9 @@
 package binaryTree;
 
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+    public int val;
+    public TreeNode left;
+     public TreeNode right;
     TreeNode() {}
     public TreeNode(int val) { this.val = val; }
     public TreeNode(int val, TreeNode left, TreeNode right) {
