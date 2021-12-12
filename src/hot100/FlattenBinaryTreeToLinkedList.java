d3410df177ab496c3830f5846ce895bd81caa4a4
ruicong <juictungwong@outlook.com>
2021-12-12 15:58:43 +0800

    二叉树展开为链表

---
+++ src/hot100/FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,23 @@
+package hot100;
+
+import binaryTree.TreeNode;
+
+public class FlattenBinaryTreeToLinkedList {
+}
+
+class Solution114 {
+    public void flatten(TreeNode root) {
+        while (root != null) {
+            if (root.left != null) {
+                TreeNode pre = root.left;
+                while (pre.right != null) {
+                    pre = pre.right;
+                }
+                pre.right = root.right;
+                root.right = root.left;
+                root.left = null;
+            }
+            root = root.right;
+        }
+    }
+}
