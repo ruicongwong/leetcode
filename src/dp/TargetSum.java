8041256238465492f7a7c0108a8e0ede3d27ad33
ruicong <juictungwong@outlook.com>
2021-12-17 16:53:15 +0800

    目标和

--- src/dp/TargetSum.java
+++ src/dp/TargetSum.java
@@ -1,10 +1,23 @@
 package dp;
 
+import java.util.Arrays;
+
 public class TargetSum {
 }
 
 class Solution494 {
     public int findTargetSumWays(int[] nums, int target) {
-
+        int sum = Arrays.stream(nums).sum();
+        if ((target + sum) % 2 != 0) return 0;
+        int bagSize = (target + sum) / 2;
+        if (bagSize < 0) bagSize = -bagSize;
+        int[] dp = new int[bagSize + 1];
+        dp[0] = 1;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = bagSize; j >= nums[i]; j--) {
+                dp[j] += dp[j - nums[i]];
+            }
+        }
+        return dp[bagSize];
     }
 }
