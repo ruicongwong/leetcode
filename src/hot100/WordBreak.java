69717c7c5d3fd994e2d5e8d79c0e96a32c5e305a
ruicong <juictungwong@outlook.com>
2021-12-18 22:20:13 +0800

    单词拆分

---
+++ src/hot100/WordBreak.java
@@ -0,0 +1,21 @@
+package hot100;
+
+import java.util.List;
+
+public class WordBreak {
+}
+
+class Solution139 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (wordDict.contains(s.substring(j, i)) && dp[j]) {
+                    dp[i] = true;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
