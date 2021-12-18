1ea4477ee315775478f7890210a2b107c1f94abb
ruicong <juictungwong@outlook.com>
2021-12-18 16:25:13 +0800

    组合总和 Ⅳ

---
+++ src/dp/CombinationSumIV.java
@@ -0,0 +1,19 @@
+package dp;
+
+public class CombinationSumIV {
+}
+
+class Solution377 {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i - nums[j] >= 0) {
+                    dp[i] += dp[i - nums[j]];
+                }
+            }
+        }
+        return dp[target];
+    }
+}
