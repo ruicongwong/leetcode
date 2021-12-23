66e377f2769fe9c7bb46a4908eb9ed707932b79f
ruicong <juictungwong@outlook.com>
2021-12-23 16:00:57 +0800

    搜索二维矩阵II

---
+++ src/hot100/SearchA2DMatrixII.java
@@ -0,0 +1,21 @@
+package hot100;
+
+public class SearchA2DMatrixII {
+}
+
+class Solution240 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0){
+            if(matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target){
+                row += 1;
+            } else {
+                col -= 1;
+            }
+        }
+        return false;
+    }
+}
