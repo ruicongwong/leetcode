89d7522a1f7c4b01f353acc23eaec9a9eb1e58c2
ruicong <juictungwong@outlook.com>
2021-12-26 16:00:49 +0800

    和为K的子数组

---
+++ src/hot100/SubarraySumEqualsK.java
@@ -0,0 +1,23 @@
+package hot100;
+
+import java.util.HashMap;
+
+public class SubarraySumEqualsK {
+}
+
+class Solution560 {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int preSum = 0;
+        int count = 0;
+        for (int num : nums) {
+            preSum += num;
+            if (map.containsKey(preSum - k)) {
+                count += map.get(preSum - k);
+            }
+            map.put(preSum, map.getOrDefault(preSum, 0) + 1);
+        }
+        return count;
+    }
+}
