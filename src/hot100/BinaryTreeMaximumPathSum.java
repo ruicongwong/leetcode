1cc249d2f374f20dce589e54ff14d827b0ee81b9
ruicong <juictungwong@outlook.com>
2022-01-02 21:38:19 +0800

    二叉树中的最大路径和

---
+++ src/hot100/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,24 @@
+package hot100;
+
+import binaryTree.TreeNode;
+
+public class BinaryTreeMaximumPathSum {
+}
+
+class Solution124 {
+    int max = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return max;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = Math.max(0, dfs(root.left));
+        int right = Math.max(0, dfs(root.right));
+        max = Math.max(max, root.val + left + right);
+        return root.val + Math.max(left, right);
+    }
+}
