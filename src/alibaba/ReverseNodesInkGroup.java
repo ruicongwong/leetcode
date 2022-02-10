bd1fabe3fd1c447cccaa63e84aa4b72ea7eeb03b
ruicong <juictungwong@outlook.com>
2022-02-10 14:14:26 +0800

    K个一组反转链表

---
+++ src/alibaba/ReverseNodesInkGroup.java
@@ -0,0 +1,41 @@
+package alibaba;
+
+import list.ListNode;
+
+public class ReverseNodesInkGroup {
+}
+
+class Solution25 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode end = dummy;
+        ListNode pre = dummy;
+        while (end.next != null) {
+            for (int i = 0; i < k && end != null; i++) {
+                end = end.next;
+            }
+            if (end == null) break;
+            ListNode start = pre.next;
+            ListNode next = end.next;
+            end.next = null;
+            pre.next = reverse(start);
+            start.next = next;
+            pre = start;
+            end = pre;
+        }
+        return dummy.next;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode cur = head;
+        ListNode pre = null;
+        while (cur != null) {
+            ListNode tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+}
