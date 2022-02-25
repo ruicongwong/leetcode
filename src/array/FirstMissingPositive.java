76808ff7a45b9a7320e1d0bf31e8f92caef86cbd
ruicong <juictungwong@outlook.com>
2022-02-25 22:00:41 +0800

    缺失的第一个正数

---
+++ src/array/FirstMissingPositive.java
@@ -0,0 +1,26 @@
+package array;
+
+public class FirstMissingPositive {
+}
+
+class Solution41 {
+    public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] >= 1 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1]) {
+                swap(nums,i, nums[i] - 1);
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1 ) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
